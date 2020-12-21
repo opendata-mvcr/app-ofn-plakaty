3a087b76fde2269e1ccfff28d44ba07577891c1c
michalskop <michalskop@users.noreply.github.com>
2020-12-21 23:22:33 +0000

    deploy: 551bdec076bd9bbd40d10563cd3f3b7882ba58c7

---
+++ precache-manifest.be7fc1d69a101a115f2f29bb1183c219.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb3948211058a7ad71e",
+    "url": "/ofnapp/css/about.729b759e.css"
+  },
+  {
+    "revision": "4ce71a52c1445e57517d",
+    "url": "/ofnapp/css/app.67ea067c.css"
+  },
+  {
+    "revision": "6c123b96ca6ec4ac822d",
+    "url": "/ofnapp/css/chunk-vendors.f32eaac2.css"
+  },
+  {
+    "revision": "3baa2c3df499cff54af11f1f609b1373",
+    "url": "/ofnapp/data/konvent.json"
+  },
+  {
+    "revision": "bed8550f7d6d2125802bee6a5c588203",
+    "url": "/ofnapp/data/lom.json"
+  },
+  {
+    "revision": "56dc94ae0287c4cc7e20ef4810458fe7",
+    "url": "/ofnapp/data/skala.json"
+  },
+  {
+    "revision": "cfaf5efc2d861cfdfa9fafc310cd6e37",
+    "url": "/ofnapp/data/spilberk.json"
+  },
+  {
+    "revision": "6e891fb3bc7f8c7d6004bee4671307ed",
+    "url": "/ofnapp/data/sportoviste_ofn.json"
+  },
+  {
+    "revision": "1b28883072ebc9b74705aaad18eef4c6",
+    "url": "/ofnapp/data/sportoviste_plasy.json"
+  },
+  {
+    "revision": "f05cfa594741588600ee5f905750a4eb",
+    "url": "/ofnapp/data/sportoviste_praha11.json"
+  },
+  {
+    "revision": "03ebc5b82724c59af194ce42610e082a",
+    "url": "/ofnapp/img/IMG_3111.jpg"
+  },
+  {
+    "revision": "e0afda1458a6962707c4ffcad338fb2d",
+    "url": "/ofnapp/index.html"
+  },
+  {
+    "revision": "deb3948211058a7ad71e",
+    "url": "/ofnapp/js/about.63cb1ba0.js"
+  },
+  {
+    "revision": "4ce71a52c1445e57517d",
+    "url": "/ofnapp/js/app.4e65601c.js"
+  },
+  {
+    "revision": "23755ef84ebe03505140",
+    "url": "/ofnapp/js/chunk-1a517a1a.de415768.js"
+  },
+  {
+    "revision": "bbe813c7ebe923dbece4",
+    "url": "/ofnapp/js/chunk-2d216214.f98ebe23.js"
+  },
+  {
+    "revision": "6c123b96ca6ec4ac822d",
+    "url": "/ofnapp/js/chunk-vendors.fad9d34b.js"
+  },
+  {
+    "revision": "bfa33208f40c0e7cc019a520904406aa",
+    "url": "/ofnapp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ofnapp/robots.txt"
+  }
+]);
